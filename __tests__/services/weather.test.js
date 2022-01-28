9b8d717fbd2548567c823b3fbdf582122216f3e7
Jonathon Gonzalez <gonzalezjonathon55@gmail.com>
2022-01-28 00:57:06 -0500

    Removed log

--- __tests__/services/weather.test.js
+++ __tests__/services/weather.test.js
@@ -4,10 +4,9 @@ const { WEATHER_API_URL, WEATHER_API_KEY } = require('../../config')
 
 
 describe('Query External Weather API by Longitude/Lattitude', () => {
-    
+
     test('Test Weather API for 200 response', async () => {
         const response = await fetch(`${WEATHER_API_URL}/?lat=${55.450047}&lon=${-73.940992}&units=imperial&appid=${WEATHER_API_KEY}`);
-        console.log(response.ok)
         expect(response.ok).toEqual(true)
     });
     
